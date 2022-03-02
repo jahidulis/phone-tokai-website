03bc01e2c1a0db7e1d6b35da8ff4116589c042ca
jahidulis <jahidulislam.exm@gmail.com>
2022-03-02 22:20:51 +0600

    done

--- js/app.js
+++ js/app.js
@@ -1,18 +1,47 @@
-const searchFood = () => {
+// spinne >>>>>
+const toggleSpinner = (displayApply, displayRemove) => {
+    document.getElementById("spinner").classList.remove(displayRemove);
+    document.getElementById("spinner").classList.add(displayApply);
+  };
+  // search result >>>>
+  const toggleSearchResult = (displayRemove, displayApply) => {
+    document.getElementById("search").classList.remove(displayRemove);
+    document.getElementById("search").classList.add(displayApply);
+  };
+  //input field >>>>
+  const searchMobile = async () => {
+    toggleSpinner("d-block", "d-none");
+    toggleSearchResult("d-block", "d-none");
     const searchField = document.getElementById("search-field");
     const searchText = searchField.value;
-    console.log(searchText);
+  
     searchField.value = "";
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
-    // console.log(url);
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => displayResult(data.data));
-  };
+
+    if (searchText != "") {
+      const response = await fetch(url);
+      const data = await response.json().then((data) => {
+        if (data.status == true) {
+          displayResult(data.data);
+        } else {
+          clearSearch();
+          toggleSpinner("d-none", "d-block");
   
+          alert("Sorry We don't have this Item Search Another One :)");
+        }
+      });
+    } else {
+      toggleSpinner("d-none", "d-block");
+      clearSearch();
+      alert("Please Enter a Value");
+    }
+  };
+  // result >>>>
   const displayResult = (data) => {
-    data.forEach((data) => console.log(data));
-    console.log(data.length);
+    clearSearch();
+    const showMore = document.getElementById("show-more");
+    showMore.classList.add("d-none");
+    // data limit 20 >>>>
     if (data.length <= 20) {
       data.forEach((data) => {
         creatElement(data);
@@ -21,47 +50,49 @@ const searchFood = () => {
       data.slice(0, 20).forEach((data) => {
         creatElement(data);
       });
-      const remove = document.getElementById("show-more");
-      remove.classList.remove("d-none");
-      remove.addEventListener("click", function () {
+      const showMore = document.getElementById("show-more");
+      showMore.classList.remove("d-none");
+      showMore.addEventListener("click", function() {
+        clearSearch();
         data.forEach((data) => {
           creatElement(data);
-          remove.classList.add("d-none");
+          showMore.classList.add("d-none");
         });
       });
     }
+    toggleSpinner("d-none", "d-block");
+    toggleSearchResult("d-none", "d-block");
   };
   const creatElement = (data) => {
     const searchResult = document.getElementById("search-result");
     const div = document.createElement("div");
     div.classList.add("col");
-  
     div.innerHTML = `<div class="card h-100 mx-auto" >
           <img src="${data.image}" class="card-img-top" alt="..."  />
-          <div class="card-body text-center">
+          <div class="card-body rounded rounded-1 text-center">
             <h5 class="card-title">${data.phone_name}</h5>
             <p class="card-text">
               <h3>${data.brand}</h3>
             </p>
-            <button onclick = "mealDetail('${data.slug}')" class="btn btn-primary">Details</button>
+            <button onclick = "mobileDetail('${data.slug}')" class="btn btn-primary">Details</button>
           </div>
         </div>`;
   
     searchResult.appendChild(div);
   };
-  
-  const mealDetail = (id) => {
+  const mobileDetail = async (id) => {
+    clearFeature();
     const url = `https://openapi.programming-hero.com/api/phone/${id}`;
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => loadDetail(data.data));
+    const response = await fetch(url);
+    const data = await response.json();
+    loadDetail(data.data);
   };
-  
   const loadDetail = (phone_detail) => {
     const showDetails = document.getElementById("show-details");
     const div = document.createElement("div");
     div.classList.add("row");
     div.classList.add("g-0");
+    // Release date >>>>
     let release = "No release date found";
     if (phone_detail.releaseDate != "") {
       release = `${phone_detail.releaseDate}`;
@@ -75,14 +106,23 @@ const searchFood = () => {
     );
   
     showDetails.appendChild(div);
-    console.log(phone_detail);
-    const mainFeature = phone_detail.mainFeatures;
-    // console.log(Object.entries(mainFeature));
-    const others = phone_detail.others;
-    console.log(others);
   
-    for (let [key, value] of Object.entries(mainFeature)){
-        key = key.toUpperCase();
+    document.getElementById("specification").classList.remove("d-none");
+    const mainFeature = phone_detail.mainFeatures;
+    const other = phone_detail.others;
+  
+    let tbody = document.getElementById("features");
+    let tr1 = document.createElement("tr");
+    tr1.classList.add("bg-dark");
+    tr1.classList.add("opacity-25");
+    tr1.classList.add("text-white");
+    tr1.innerHTML =
+      "<td colspan='2'><h3 class='text-center'>Main Features</h3></td>";
+  
+    tbody.appendChild(tr1);
+  
+    for (let [key, value] of Object.entries(mainFeature)) {
+      key = key.toUpperCase();
       if (typeof value == "object") {
         let ul = document.createElement("ul");
         let liStr = "";
@@ -101,7 +141,7 @@ const searchFood = () => {
         document.getElementById("features").append(tr);
       }
     }
-    if (others != "") {
+    if (other != "" && other != undefined) {
       let tbody = document.getElementById("features");
       let tr2 = document.createElement("tr");
       tr2.classList.add("bg-dark");
@@ -111,7 +151,7 @@ const searchFood = () => {
   
       tbody.appendChild(tr2);
   
-      for (let [key, value] of Object.entries(others)) {
+      for (let [key, value] of Object.entries(other)) {
         key = key.toUpperCase();
         const tr = document.createElement("tr");
         tr.innerHTML = `<td scope="row"><strong>${key}</strong></td>
@@ -121,15 +161,33 @@ const searchFood = () => {
     }
   };
   const mainInnerHtml = (key, value) => {};
+  //Card >>>>
   const innerHTMLset = (img, name, brand, releaseDate) => {
     return `<div class="col-md-4">
     <img class="d-block mx-auto" src="${img}" class="img-fluid rounded-start" alt="...">
-  </div>
-  <div class="col-md-8">
+    </div>
+     <div class="col-md-8">
     <div class="card-body text-center">
-      <h3 class="card-title">${name}</h3>
+      <h3 class="card-title ps-2">${name}</h3>
       <p class="card-text"><h4>${brand}</h4> </p>
       <p class="card-text"><small class="text-muted">${releaseDate}</small></p>
     </div>
   </div>`;
   };
+  // Clear Item & fetures >>>>
+  function clearFeature() {
+    const showDetails = document.getElementById("show-details");
+    showDetails.textContent = "";
+    document.getElementById("specification").classList.add("d-none");
+    let tbody = document.getElementById("features");
+    tbody.textContent = "";
+  }
+  //Clear Search Things >>>>
+  function clearSearch() {
+    const showMore = document.getElementById("show-more");
+    showMore.classList.add("d-none");
+    const searchResult = document.getElementById("search-result");
+    searchResult.textContent = "";
+  
+    clearFeature();
+  }
